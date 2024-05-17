e64201a9b54b6068ab5149151d2a13a765329d57
ayushkrl <111305534+ayushkrl@users.noreply.github.com>
2024-05-17 13:30:06 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+// typescript
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// == aur comparisions work differently
+// comparisions convert null to number, treating it as 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+console.log("2" === 2);
+
