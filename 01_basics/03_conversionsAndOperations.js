784892b7a7cd197750bc249af94e43a5e0d7ef25
ayushkrl <111305534+ayushkrl@users.noreply.github.com>
2024-05-17 03:08:44 +0000

    Conversions

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -72,3 +72,41 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ------------------- operations --------------------- 
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2**2) [+ - * / ** %]
+
+let str1 = "ayush"
+let str2 = " haha"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+
+// console.log("1" + 2 + 2);
+// console.log((1 + 2) + "2");
+
+// conersion 30 min vid
+
+console.log(+true);
+// console.log(true+);  error
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+++gameCounter; // prefix / postfix operator
+console.log(gameCounter);
+
+// type conversion - https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
